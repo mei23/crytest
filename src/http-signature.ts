049dad04a521bdf6c849f77d8a6bf2b1f045ca49
mei23 <m@m544.net>
2021-06-13 17:54:51 +0900

    wip verify

--- src/http-signature.ts
+++ src/http-signature.ts
@@ -16,6 +16,56 @@ export type SignatureKeyAlgorithm = 'rsa' | 'ecdsa' | 'ed25519' | 'ed448';
 export type SignatureHashAlgorithm = 'sha1' | 'sha256' | 'sha512';
 export type SignatureAlgorithm = 'rsa-sha1' | 'rsa-sha256' | 'rsa-sha512' | 'ecdsa-sha1' | 'ecdsa-sha256' | 'ecdsa-sha512';
 
+type ParsedSignature = {
+	scheme: 'Signature';
+	params: {
+		keyId: string;
+		algorithm?: string;	// 'rsa-sha256'
+		headers: string[];	//[ '(request-target)', 'date', 'host', 'digest' ],
+		signature: string;
+	};
+	signingString: string;
+	algorithm?: string;	// 'RSA-SHA256'
+	keyId: string;
+};
+
+export class HttpSignatureVerifier {
+	private publicKeyPem: string;
+
+	constructor(publicKeyPem: string) {
+		this.publicKeyPem = publicKeyPem;
+	}
+
+	public verify(parsed: ParsedSignature) {
+		let legacyKeyAlg: string | undefined;
+		let legacyHashAlg: string | undefined;
+		const m = parsed.params.algorithm?.match(/^(rsa|hmac|ecdsa)-(sha(?:1|256|512))$/);
+		if (m) {
+			legacyKeyAlg = m[1],
+			legacyHashAlg = m[2];
+		}
+
+		const k = crypto.createPublicKey(this.publicKeyPem);
+
+		if (legacyKeyAlg) {
+			if (k.asymmetricKeyType === 'rsa' && legacyKeyAlg !== 'rsa') throw 'rsa';
+			if (k.asymmetricKeyType === 'ec' && legacyKeyAlg !== 'ecdsa') throw 'ec';
+			if (k.asymmetricKeyType === 'ed25519') throw 'ed25519';
+			if (k.asymmetricKeyType === 'ed448') throw 'ed448';
+		}
+
+		if (k.asymmetricKeyType === 'ed25519' || k.asymmetricKeyType === 'ed448') {
+			return crypto.verify(null, Buffer.from(parsed.signingString), this.publicKeyPem, Buffer.from(parsed.params.signature, 'base64'));
+		} else if (k.asymmetricKeyType === 'rsa') {
+			return crypto.verify(legacyHashAlg || 'sha256', Buffer.from(parsed.signingString), this.publicKeyPem, Buffer.from(parsed.params.signature, 'base64'));
+		} else if (k.asymmetricKeyType === 'ec') {
+			return crypto.verify(legacyHashAlg, Buffer.from(parsed.signingString), this.publicKeyPem, Buffer.from(parsed.params.signature, 'base64'));
+		} else {
+			throw 'unsupported';
+		}
+	}
+}
+
 export class HttpSignature {
 	private key: SignatureKey;
 	public hashAlgorithm: SignatureHashAlgorithm = 'sha256';
---
+++ test/http-signature-verify.ts
@@ -0,0 +1,83 @@
+import * as assert from 'assert';
+import { HttpSignature, HttpSignatureVerifier } from '../src/http-signature';
+import { genRsaKeyPair, genEcKeyPair, genEd25519KeyPair, genEd448KeyPair } from '../src/keypair';
+
+type ParsedSignature = {
+	scheme: 'Signature';
+	params: {
+		keyId: string;
+		algorithm: string;	// 'rsa-sha256'
+		headers: string[];	//[ '(request-target)', 'date', 'host', 'digest' ],
+		signature: string;
+	};
+	signingString: string;
+	algorithm: string;	// 'RSA-SHA256'
+	keyId: string;
+};
+
+const buildParsedSignature = (signingString: string, signature: string, algorithm: string) => {
+	return {
+		scheme: 'Signature',
+		params: {
+			keyId: 'KeyID',
+			algorithm: algorithm,
+			headers: [ '(request-target)', 'date', 'host', 'digest' ],
+			signature: signature,
+		},
+		signingString: signingString,
+		algorithm: algorithm?.toUpperCase(),
+		keyId: 'KeyID',
+	} as ParsedSignature;
+};
+
+describe('HTTP Signature verify by joyent', () => {
+	it('rsa-sha256 2048', async () => {
+		const keyPair = await genRsaKeyPair(2048);
+		const signingString = 'foo';
+		const signature = HttpSignature.genSignature(signingString, keyPair.privateKey, 'sha256');
+		const parsed = buildParsedSignature(signingString, signature, 'rsa-sha256');
+		const v = new HttpSignatureVerifier(keyPair.publicKey);
+		const result = v.verify(parsed);
+		assert.deepStrictEqual(result, true);
+	});
+
+	it('rsa-sha256 2048 2', async () => {
+		const keyPair = await genRsaKeyPair(2048);
+		const signingString = 'foo';
+		const signature = HttpSignature.genSignature(signingString, keyPair.privateKey, 'sha256');
+		const parsed = buildParsedSignature(signingString, signature, undefined);
+		const v = new HttpSignatureVerifier(keyPair.publicKey);
+		const result = v.verify(parsed);
+		assert.deepStrictEqual(result, true);
+	});
+
+	it('ecdsa-sha512', async () => {
+		const keyPair = await genEcKeyPair();
+		const signingString = 'foo';
+		const signature = HttpSignature.genSignature(signingString, keyPair.privateKey, 'sha512');
+		const parsed = buildParsedSignature(signingString, signature, 'ecdsa-sha512');
+		const v = new HttpSignatureVerifier(keyPair.publicKey);
+		const result = v.verify(parsed);
+		assert.deepStrictEqual(result, true);
+	});
+
+	it('ed25519', async () => {
+		const keyPair = await genEd25519KeyPair();
+		const signingString = 'foo';
+		const signature = HttpSignature.genEdSignature(signingString, keyPair.privateKey);
+		const parsed = buildParsedSignature(signingString, signature, undefined);
+		const v = new HttpSignatureVerifier(keyPair.publicKey);
+		const result = v.verify(parsed);
+		assert.deepStrictEqual(result, true);
+	});
+
+	it('ed448', async () => {
+		const keyPair = await genEd448KeyPair();
+		const signingString = 'foo';
+		const signature = HttpSignature.genEdSignature(signingString, keyPair.privateKey);
+		const parsed = buildParsedSignature(signingString, signature, undefined);
+		const v = new HttpSignatureVerifier(keyPair.publicKey);
+		const result = v.verify(parsed);
+		assert.deepStrictEqual(result, true);
+	});
+});
