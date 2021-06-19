2851fe9be4d039542f25c2ee193b6b20b7208158
mei23 <m@m544.net>
2021-06-20 01:53:55 +0900

    js

--- test/crypto.ts
+++ test/crypto.ts
@@ -1,13 +1,12 @@
 //import * as assert from 'assert';
 import * as crypto from 'crypto';
-import { inspect } from 'util';
 
 describe('crypto', () => {
 	it('getHashes()', async () => {
-		console.log(inspect(crypto.getHashes()));
+		console.log(JSON.stringify(crypto.getHashes()));
 	});
 
 	it('getCurves()', async () => {
-		console.log(inspect(crypto.getCurves()));
+		console.log(JSON.stringify(crypto.getCurves()));
 	});
 });
