0e2413d62772744fd677d911fd43d754b5365047
mei23 <m@m544.net>
2023-12-04 08:52:15 +0900

    swc

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	transform: {
+		"^.+\\.(t|j)sx?$": "@swc/jest",
+	},
+	testEnvironment: 'node',
 	testPathIgnorePatterns: ['/node_modules/', 'utils'],
 };
